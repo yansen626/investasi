e719c9599d6765e793057bc6a4a99e50e6a2f51e
Bayu <bayuindra091191@gmail.com>
2018-04-01 15:06:15 +0700

    Blade email status proyek

---
+++ resources/views/email/proyek-terpenuhi.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="initial-scale=1.0" name="viewport">
+    <meta content="telephone=no" name="format-detection">
+    <title>Indofund.id - Prospectus</title>
+    <style type="text/css">
+        .socialLinks {font-size: 6px;}
+        .socialLinks a {display: inline-block;}
+        .socialIcon {display: inline-block;vertical-align: top;padding-bottom: 0px;border-radius: 100%;}
+        table.vb-row, table.vb-content {border-collapse: separate;}
+        table.vb-row {border-spacing: 9px;}
+        table.vb-row.halfpad {border-spacing: 0;padding-left: 9px;padding-right: 9px;}
+        table.vb-row.fullwidth {border-spacing: 0;padding: 0;}
+        table.vb-container.fullwidth {padding-left: 0;padding-right: 0;}
+    </style>
+    <style type="text/css">
+        /* yahoo, hotmail */
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div{ line-height: 100%; }
+        .yshortcuts a{ border-bottom: none !important; }
+        .vb-outer{ min-width: 0 !important; }
+        .RMsgBdy, .ExternalClass{
+            width: 100%;
+            background-color: #3f3f3f;
+            background-color: #3f3f3f}
+
+        /* outlook */
+        table{ mso-table-rspace: 0pt; mso-table-lspace: 0pt; }
+        #outlook a{ padding: 0; }
+        img{ outline: none; text-decoration: none; border: none; -ms-interpolation-mode: bicubic; }
+        a img{ border: none; }
+
+        @media screen and (max-device-width: 600px), screen and (max-width: 600px) {
+            table.vb-container, table.vb-row{
+                width: 95% !important;
+            }
+
+            .mobile-hide{ display: none !important; }
+            .mobile-textcenter{ text-align: center !important; }
+
+            .mobile-full{
+                float: none !important;
+                width: 100% !important;
+                max-width: none !important;
+                padding-right: 0 !important;
+                padding-left: 0 !important;
+            }
+            img.mobile-full{
+                width: 100% !important;
+                max-width: none !important;
+                height: auto !important;
+            }
+        }
+    </style>
+    <style type="text/css">
+        #ko_textBlock_4 .links-color a:visited, #ko_textBlock_4 .links-color a:hover {color: #3f3f3f;color: #3f3f3f;text-decoration: underline;}
+        #ko_footerBlock_2 .links-color a, #ko_footerBlock_2 .links-color a:link, #ko_footerBlock_2 .links-color a:visited, #ko_footerBlock_2 .links-color a:hover {color: #ccc;color: #ccc;text-decoration: underline;}
+    </style>
+</head>
+<body alink="#CCCCCC" bgcolor="#3F3F3F" style="margin: 0;padding: 0;background-color: #3f3f3f;color: #919191;" text="#919191" vlink="#CCCCCC">
+<center>
+    <!-- preheaderBlock -->
+    <!-- /preheaderBlock -->
+    <table bgcolor="#fff" border="0" cellpadding="0" cellspacing="0" class="vb-outer" id="ko_logoBlock_5" style="background-color: #fff;" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" bgcolor="#fff" class="vb-outer" style="padding-left: 9px;padding-right: 9px;background-color: #fff;" valign="top">
+                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+                <div class="oldwebkit" style="max-width: 570px;">
+                    <table border="0" cellpadding="0" cellspacing="18" class="vb-container fullpad" style="border-collapse: separate;border-spacing: 18px;padding-left: 0;padding-right: 0;width: 100%;max-width: 570px;" width="570">
+                        <tbody>
+                        <tr>
+                            <td align="center" valign="top">
+                                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="166"><tr><td align="center" valign="top"><![endif]-->
+                                <div class="mobile-full" style="display: inline-block; max-width: 166px; vertical-align: top; width: 100%;">
+                                    <a href="www.indofund.id" style="font-size: 18px; font-family: Arial, Helvetica, sans-serif; color: #f3f3f3; text-decoration: none;" target="_new"><img alt="" border="0" hspace="0" src="https://mosaico.io/srv/f-dtn0k95/img?src=https%3A%2F%2Fmosaico.io%2Ffiles%2Fdtn0k95%2Flogo%2520%25282%2529.png&amp;method=resize&amp;params=350%2Cnull" style="border: 0px;display: block;width: 100%;max-width: 166px;" vspace="0" width="166"></a>
+                                </div><!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div><!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <table bgcolor="#fff" border="0" cellpadding="0" cellspacing="0" class="vb-outer" id="ko_titleBlock_3" style="background-color: #fff;" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" bgcolor="#fff" class="vb-outer" style="padding-left: 9px;padding-right: 9px;background-color: #fff;" valign="top">
+                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+                <div class="oldwebkit" style="max-width: 650px;">
+                    <table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="9" class="vb-container halfpad" style="border-collapse: separate;border-spacing: 9px;padding-left: 9px;padding-right: 9px;width: 100%;max-width: 650px;background-color: #fff;" width="650">
+                        <tbody>
+                        <tr>
+                            <td align="center" bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 22px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; text-align: center;"><span>Halo, berikut ini proyek anda berhasil terpenuhi 100%.</span></td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 16px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; text-align: left;">
+                                <ul>
+                                    <li><a style="text-decoration: underline;" href="#">Project 1</a></li>
+                                    <li><a style="text-decoration: underline;" href="#">Project 2</a></li>
+                                    <li><a style="text-decoration: underline;" href="#">Project 3</a></li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 16px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; text-align: center;"><span>Klik di <a style="text-decoration: underline;" href="#">sini</a> atau pada masing-masing proyek untuk informasi lebih detil.</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div><!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table><!-- footerBlock -->
+    <table bgcolor="#3F3F3F" border="0" cellpadding="0" cellspacing="0" id="ko_footerBlock_2" style="background-color: #3f3f3f;" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" bgcolor="#3F3F3F" style="background-color: #3f3f3f;" valign="top">
+                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+                <div class="oldwebkit" style="max-width: 570px;">
+                    <table align="center" border="0" cellpadding="0" cellspacing="9" class="vb-container halfpad" style="border-collapse: separate;border-spacing: 9px;padding-left: 9px;padding-right: 9px;width: 100%;max-width: 570px;" width="570">
+                        <tbody>
+                        <tr>
+                            <td class="long-text links-color" style="text-align: center; font-size: 13px; color: #919191; font-weight: normal; text-align: center; font-family: Verdana, Geneva, sans-serif;">
+                                <p style="margin: 1em 0px;margin-bottom: 0px;margin-top: 0px; color: #fff;">indofund.id @ 2017&nbsp;All rights reserved.</p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div><!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table><!-- /footerBlock -->
+</center>
+</body>
+</html>
